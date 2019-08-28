294e545bf9a1e246910c43b9b4ab2d5f95643a8c
ChenxiiCheng <chenxic1011@gmail.com>
2019-08-28 15:28:38 +0800

    updated .gitignore

---
+++ build/precache-manifest.cff7ebbd02d863bbe4861bf2dba59383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18106e0235b075e666963e586a873f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb8bb690941a9b13c738",
+    "url": "./static/css/2.bc9934ea.chunk.css"
+  },
+  {
+    "revision": "7045032f87e5b7edbbfc",
+    "url": "./static/css/main.0526bb1f.chunk.css"
+  },
+  {
+    "revision": "eb8bb690941a9b13c738",
+    "url": "./static/js/2.d8b25dee.chunk.js"
+  },
+  {
+    "revision": "7045032f87e5b7edbbfc",
+    "url": "./static/js/main.c08093c2.chunk.js"
+  },
+  {
+    "revision": "d4321217549890ea1262",
+    "url": "./static/js/runtime~main.d52498a0.js"
+  }
+]);
